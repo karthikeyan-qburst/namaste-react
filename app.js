1b72d8fab423a8de6536e9278b1af4a95ad46cc8
karthikeyan <karthikeyank@qburst.com>
2024-02-20 11:48:43 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("h1", {className:"heading"}, "Hello world from app.js");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
