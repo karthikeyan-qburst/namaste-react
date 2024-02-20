460bb6e9503f0f3293a44a1a94fd700ac186d457
karthikeyan <karthikeyank@qburst.com>
2024-02-20 15:04:08 +0530

    episode-03

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", {className:"heading"}, "Namaste react world from app.js");
+const heading = <h1 className="heading">Namaste React with JSX2</h1>
+const Heading = () => (
+    <div>
+        <h1>{console.log("hey")}</h1>
+        <h1 className="heading">Namaste React with Functional Component</h1>
+    </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<Heading />);
