eb12769d13871cecefd1f1ca17699f228cc41e0f
karthikeyan <karthikeyank@qburst.com>
2024-02-20 13:58:37 +0530

    episode-02

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Namaste React 
+
+#Parcel
+- Dev build
+- Local server
+- HMR Hot Module Reload
+- File watching algorithm
+- Faster builds using caching
+- Image optimisation
+- minification
+- Differential bundling
+- error handling
+- tree shaking
+- https support
+- transpilation
+- web server
+- code splitting
--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-const heading = React.createElement("h1", {className:"heading"}, "Hello world from app.js");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("h1", {className:"heading"}, "Namaste react world from app.js");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
